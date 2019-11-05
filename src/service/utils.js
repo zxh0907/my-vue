49ffff9319acda362ef06720f58c6ef6fcda6c22
unknown <zhangxiaohui@360.cn>
2019-11-05 15:32:22 +0800

    utils

--- src/service/utils.js
+++ src/service/utils.js
@@ -1,3 +1,26 @@
+//两个json做比较，返回不同的部分
+function jsonDiff(obj1, obj2) {
+    const result = {};
+    if (Object.is(obj1, obj2)) {
+        return undefined;
+    }
+    if (!obj2 || typeof obj2 !== 'object') {
+        return obj2;
+    }
+    Object.keys(obj1 || {}).concat(Object.keys(obj2 || {})).forEach(key => {
+        if(obj2[key] !== obj1[key] && !Object.is(obj1[key], obj2[key])) {
+            result[key] = obj2[key];
+        }
+        if(typeof obj2[key] === 'object' && typeof obj1[key] === 'object') {
+            const value = jsonDiff(obj1[key], obj2[key]);
+            if (value !== undefined) {
+                result[key] = value;
+            }
+        }
+    });
+    return result;
+}
+
 export default {
 	browser: {
     device: function() {
@@ -154,25 +177,5 @@ export default {
     }
   },
   //两个json做比较，返回不同的部分
-  jsonDiff(obj1, obj2) {
-    const result = {};
-    if (Object.is(obj1, obj2)) {
-        return undefined;
-    }
-    if (!obj2 || typeof obj2 !== 'object') {
-        return obj2;
-    }
-    Object.keys(obj1 || {}).concat(Object.keys(obj2 || {})).forEach(key => {
-        if(obj2[key] !== obj1[key] && !Object.is(obj1[key], obj2[key])) {
-            result[key] = obj2[key];
-        }
-        if(typeof obj2[key] === 'object' && typeof obj1[key] === 'object') {
-            const value = diff(obj1[key], obj2[key]);
-            if (value !== undefined) {
-                result[key] = value;
-            }
-        }
-    });
-    return result;
-}
+  jsonDiff
 }
